b5a0b325f14bb510d3bf8cf50bcd6357fd3f9ac1
TurBoss <j.l.toledano.l@gmail.com>
2021-10-07 00:12:39 +0200

    add char and animations

--- scripts/kineball.gd
+++ scripts/kineball.gd
@@ -3,28 +3,75 @@ extends KinematicBody
 #var rand_generate = RandomNumberGenerator.new()
 var vec_pos = Vector3(0,0,0) 
 var speed = 20
-var rotspeed= 9
-var gravity= -5
+var rotspeed = 9
+var gravity = -5
 #var wind = 0.0
 var jump_force = 105
 
 var max_jumps = 3
 var jumps_count = 0
 
+var player_rotation = 0
+var rotating_left = false
+var rotating_right = false
+
 func _ready():
 	pass
-	
+
+# Physics Loop
 func _physics_process(_delta):
+	
+	
+	var y = 0
+	
+	# input handler
 	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
 		vec_pos.z=0
+		
+	elif Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_down"):
+		vec_pos.z=-speed
+		
+		$personaje3/AnimationPlayer.play("crouch_walk_forward-loop")
+		
+	elif Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_down"):
+		vec_pos.z=speed
+		
+		$personaje3/AnimationPlayer.play("crouch_walk_back-loop")
+		
 	elif Input.is_action_pressed("ui_right"):
 		vec_pos.z=-speed
-		$model/AnimationPlayer.play("walkAction")
+		if $personaje3.rotation.y <= 90 and $personaje3.rotation.y <= 0:
+			rotating_right = true
+			player_rotation = _delta * rotspeed
+			$personaje3.rotate(Vector3(0, 1, 0), player_rotation)
+			
+		$personaje3/AnimationPlayer.play("walk_forward-loop")
+		
 	elif Input.is_action_pressed("ui_left"):
-		$model/AnimationPlayer.play_backwards("walkAction")
 		vec_pos.z=speed
-	else:
+		if $personaje3.rotation.y >= 0 and $personaje3.rotation.y <= 90:
+			rotating_left = true
+			player_rotation = _delta * rotspeed
+			$personaje3.rotate(Vector3(0, 1, 0), player_rotation)
+		$personaje3/AnimationPlayer.play("walk_backward-loop")
+		
+	elif Input.is_action_pressed("ui_down"):
+		$personaje3/AnimationPlayer.play("crouch-loop")
 		vec_pos.z= lerp(vec_pos.z,0,0.2)
+		
+	else:
+		$personaje3/AnimationPlayer.play("fall_down-loop")
+		vec_pos.z= lerp(vec_pos.z,0,0.2)
+	
+	if $personaje3.rotation.y >= 0 and rotating_right == true:
+		rotating_right = false
+	elif $personaje3.rotation.y <= 180 and rotating_left == true:
+		rotating_left = false
+		
+	if rotating_right == true:
+		$personaje3.rotate(Vector3(0, 1, 0), _delta * rotspeed)
+	elif rotating_left == true:
+		$personaje3.rotate(Vector3(0, 1, 0), _delta * rotspeed)
 	
 	# wind = rand_generate.randf_range(-1.0, 1.0)
 	
