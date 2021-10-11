cc5f3c6b58db5a2e6730942c532e89c3a93f9dca
TurBoss <j.l.toledano.l@gmail.com>
2021-10-11 23:30:18 +0200

    main menu sfx

--- scripts/options.gd
+++ scripts/options.gd
@@ -3,18 +3,32 @@ extends Control
 func _ready():
 	pass # Replace with function body.
 
+
+
+func _on_game_options_button_mouse_entered():
+	$menu_sfx.play()
+
+func _on_video_options_button_mouse_entered():
+	$menu_sfx.play()
+
+func _on_audio_options_button_mouse_entered():
+	$menu_sfx.play()
+
+func _on_back_options_button_mouse_entered():
+	$menu_sfx.play()
+
+
 func _on_game_options_button_pressed():
 	pass # Replace with function body.
-
 
 func _on_video_options_button_pressed():
 	get_tree().change_scene("res://scenes/video.tscn")
 
-
 func _on_audio_options_button_pressed():
 	pass # Replace with function body.
 
-
 func _on_back_options_button_pressed():
 	get_tree().change_scene("res://scenes/control.tscn")
+
+
 
