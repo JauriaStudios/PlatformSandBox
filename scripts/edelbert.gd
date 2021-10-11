d35403ec80ebb8982eb505536ecbe18ec6e5820e
TurBoss <j.l.toledano.l@gmail.com>
2021-10-12 00:21:19 +0200

    fix edelbert script animation stuff

--- scripts/edelbert.gd
+++ scripts/edelbert.gd
@@ -1,15 +1,21 @@
 extends Spatial
 
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	# Play standing forever
 	var anim = $AnimationPlayer.get_animation("standing")
 	anim.set_loop(true)
 	
 	$AnimationPlayer.play("standing")
-	pass # Replace with function body.
 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-func _on_Button7_pressed():
-	get_tree().change_scene("res://scenes/Control.tscn")
