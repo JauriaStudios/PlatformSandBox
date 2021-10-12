6698d71ca3cdfa975080f647e0bf407b3cae0933
TurBoss <j.l.toledano.l@gmail.com>
2021-10-12 06:54:47 +0200

    add basic lobby

--- scripts/lobby.gd
+++ scripts/lobby.gd
@@ -41,36 +41,3 @@ func _fadeout():
 		fadeout_timer.stop()
 		return
 	material_cutoff -= 0.1
-
-func _on_game_button_mouse_entered():
-	$menu_sfx.play()
-
-func _on_lobby_button_mouse_entered():
-	$menu_sfx.play()
-
-func _on_options_button_mouse_entered():
-	$menu_sfx.play()
-
-func _on_quit_button_mouse_entered():
-	$menu_sfx.play()
-
-func _on_game_button_pressed():
-	fadeout_timer.start()
-	get_tree().change_scene("res://scenes/stage_1.tscn")
-
-func _on_lobby_button_pressed():
-	fadeout_timer.start()
-	get_tree().change_scene("res://scenes/lobby.tscn")
-
-func _on_options_button_pressed():
-	fadeout_timer.start()
-	get_tree().change_scene("res://scenes/options.tscn")
-
-func _on_quit_button_pressed():
-	fadeout_timer.start()
-	get_tree().set_auto_accept_quit(false)
-	get_tree().quit()
-
-
-
-
