ab3d59fdd4d28e7bad135bd39532bb70f5ca1845
TurBoss <j.l.toledano.l@gmail.com>
2021-10-12 05:19:51 +0200

    Add menu glow effects

--- scripts/options.gd
+++ scripts/options.gd
@@ -1,8 +1,45 @@
 extends Control
 
-func _ready():
-	pass # Replace with function body.
+var fadein_timer
+var fadeout_timer
 
+var rect_material
+var material_cutoff = 0.0
+
+func _init():
+	
+	fadein_timer = Timer.new()
+	add_child(fadein_timer)
+	fadein_timer.autostart = false
+	fadein_timer.wait_time = 0.05
+	fadein_timer.connect("timeout", self, "_fadein")
+	
+	fadeout_timer = Timer.new()
+	add_child(fadeout_timer)
+	fadeout_timer.autostart = false
+	fadeout_timer.wait_time = 0.05
+	fadeout_timer.connect("timeout", self, "_fadeout")
+
+
+func _ready():
+	$fade_in.visible = true
+	rect_material = $fade_in.get_material()
+	fadein_timer.start()
+	
+func _fadein():
+	rect_material.set_shader_param("cutoff", material_cutoff)
+	if (material_cutoff == 1.0):
+		fadein_timer.stop()
+		return
+		
+	material_cutoff += 0.1
+
+func _fadeout():
+	rect_material.set_shader_param("cutoff", material_cutoff)
+	if (material_cutoff == 0.0):
+		fadeout_timer.stop()
+		return
+	material_cutoff -= 0.1
 
 
 func _on_game_options_button_mouse_entered():
--- scripts/video.gd
+++ scripts/video.gd
@@ -1,16 +1,46 @@
 extends Control
 
 var settings = {"width":1024,"height":720,"fullscreen":false}
+var fadein_timer
+var fadeout_timer
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var rect_material
+var material_cutoff = 0.0
+
+func _init():
+	
+	fadein_timer = Timer.new()
+	add_child(fadein_timer)
+	fadein_timer.autostart = false
+	fadein_timer.wait_time = 0.05
+	fadein_timer.connect("timeout", self, "_fadein")
+	
+	fadeout_timer = Timer.new()
+	add_child(fadeout_timer)
+	fadeout_timer.autostart = false
+	fadeout_timer.wait_time = 0.05
+	fadeout_timer.connect("timeout", self, "_fadeout")
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$fade_in.visible = true
+	rect_material = $fade_in.get_material()
+	fadein_timer.start()
+	
+func _fadein():
+	rect_material.set_shader_param("cutoff", material_cutoff)
+	if (material_cutoff == 1.0):
+		fadein_timer.stop()
+		return
+		
+	material_cutoff += 0.1
 
+func _fadeout():
+	rect_material.set_shader_param("cutoff", material_cutoff)
+	if (material_cutoff == 0.0):
+		fadeout_timer.stop()
+		return
+	material_cutoff -= 0.1
 func _on_CheckBox_toggled(button_pressed):
 	settings["fullscreen"] = button_pressed
 
