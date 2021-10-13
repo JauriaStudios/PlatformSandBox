f6e242c8b53de49c5dfd7a6bf18055bd8ca0e381
TurBoss <j.l.toledano.l@gmail.com>
2021-10-13 14:28:49 +0200

    menu

--- scripts/lobby.gd
+++ scripts/lobby.gd
@@ -41,3 +41,23 @@ func _fadeout():
 		fadeout_timer.stop()
 		return
 	material_cutoff -= 0.1
+
+
+func _on_host_button_mouse_entered():
+	$menu_sfx.play()
+
+
+func _on_client_button_mouse_entered():
+	$menu_sfx.play()
+
+
+func _on_options_button_mouse_entered():
+	$menu_sfx.play()
+
+
+func _on_back_button_mouse_entered():
+	$menu_sfx.play()
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene("res://scenes/control.tscn")
