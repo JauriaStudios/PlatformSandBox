47c4fa227744204b2730e810099e09ee4f7ab2b3
TurBoss <j.l.toledano.l@gmail.com>
2021-10-14 07:44:37 +0200

    add logo and som cloths to the girl

--- scripts/player.gd
+++ scripts/player.gd
@@ -55,9 +55,9 @@ func _physics_process(_delta):
 	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
 		vec_pos.z=0
 	
-	elif Input.is_action_pressed("run") and Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("run") and Input.is_action_pressed("ui_left"):
 		
-		vec_pos.z = -speed * run_mult
+		vec_pos.z = speed * run_mult
 		if $personaje3.rotation.y <= 90 and $personaje3.rotation.y <= 0:
 			rotating_right = true
 			player_rotation = _delta * rotspeed
@@ -65,8 +65,8 @@ func _physics_process(_delta):
 		if jumps_count == 0:
 			$personaje3/AnimationPlayer.play("run_forward-loop")
 	
-	elif Input.is_action_pressed("run") and Input.is_action_pressed("ui_left"):
-		vec_pos.z = speed * run_mult
+	elif Input.is_action_pressed("run") and Input.is_action_pressed("ui_right"):
+		vec_pos.z = -speed * run_mult
 		if $personaje3.rotation.y >= 0 and $personaje3.rotation.y <= 90:
 			rotating_left = true
 			player_rotation = _delta * rotspeed
@@ -75,8 +75,8 @@ func _physics_process(_delta):
 			$personaje3/AnimationPlayer.play("run_forward-loop")
 
 	# Crouch Right
-	elif Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_down"):
-		vec_pos.z=-speed
+	elif Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_down"):
+		vec_pos.z = speed
 		if $personaje3.rotation.y <= 90 and $personaje3.rotation.y <= 0:
 			rotating_right_crouch = true
 			player_rotation = _delta * rotspeed
@@ -85,8 +85,8 @@ func _physics_process(_delta):
 			$personaje3/AnimationPlayer.play("crouch_walk_forward-loop")
 		
 	# Crouch Left
-	elif Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_down"):
-		vec_pos.z=speed
+	elif Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_down"):
+		vec_pos.z = -speed
 		if $personaje3.rotation.y >= 0 and $personaje3.rotation.y <= 90:
 			rotating_left_crouch = true
 			player_rotation = _delta * rotspeed
@@ -95,8 +95,8 @@ func _physics_process(_delta):
 			$personaje3/AnimationPlayer.play("crouch_walk_forward-loop")
 	
 	# Move Right
-	elif Input.is_action_pressed("ui_right"):
-		vec_pos.z=-speed
+	elif Input.is_action_pressed("ui_left"):
+		vec_pos.z = speed
 		if $personaje3.rotation.y <= 90 and $personaje3.rotation.y <= 0:
 			rotating_right = true
 			player_rotation = _delta * rotspeed
@@ -106,8 +106,8 @@ func _physics_process(_delta):
 			$personaje3/AnimationPlayer.play("walk_forward-loop")
 	
 	# Move Left
-	elif Input.is_action_pressed("ui_left"):
-		vec_pos.z=speed
+	elif Input.is_action_pressed("ui_right"):
+		vec_pos.z = -speed
 		if $personaje3.rotation.y >= 0 and $personaje3.rotation.y <= 90:
 			rotating_left = true
 			player_rotation = _delta * rotspeed
@@ -131,7 +131,7 @@ func _physics_process(_delta):
 	
 	else:
 		$personaje3/AnimationPlayer.play("idle-loop")
-		vec_pos.z= lerp(vec_pos.z,0,0.2)
+		vec_pos.z = lerp(vec_pos.z,0,0.2)
 		
 	
 	# Stop rotation when player in position
@@ -169,54 +169,54 @@ func _physics_process(_delta):
 	vec_pos = move_and_slide(Vector3(0, vec_pos.y, vec_pos.z),Vector3.UP)
 
 
-func _on_Abyss_body_entered(body):
-	if body.name == "KineBall":
-		get_tree().change_scene("res://scenes/Level.tscn")
-
-
-func _on_Enemy_body_entered(body):
-	if body.name == "KineBall":
-		get_parent().get_node("Sounds/hurt").play()
-		$Timer2.start()
-
-
-func _on_finish_area_body_entered(body):	
-	get_parent().get_node("Sounds/win").play()
-	$Timer.start()
-
-
-func _on_Timer_timeout():
-	get_tree().change_scene("res://scenes/Control.tscn")
-
-
-func _on_Timer2_timeout():
-	get_tree().change_scene("res://scenes/Level.tscn")
-
-
-func _on_Coin_cn_collected():
-	get_parent().get_node("Sounds/coinsnd").play()
-
-
-func _on_t1_body_entered(body):
-	if body.name == "KineBall":
-		get_parent().get_node("Doors/door1/door1animp").play("d1animp")
-#		
-		var material = get_parent().get_node("triggers/t1/MeshInstance").get_surface_material(0)
-		material.albedo_color = Color("14ff00")
-		get_parent().get_node("triggers/t1/MeshInstance").set_surface_material(0, material)
-		
-		get_parent().get_node("triggers/t1").set_collision_layer_bit(0,false)
-		get_parent().get_node("triggers/t1").set_collision_mask_bit(0,false)
-		
-
-
-func _on_t2_body_entered(body):
-	get_parent().get_node("Doors/door2/leftside/AnimationPlayer").play("d2lsanimp")
-	get_parent().get_node("Doors/door2/rightside/AnimationPlayer").play("d2rsanimp")
-	
-	var material = get_parent().get_node("triggers/t2/MeshInstance").get_surface_material(0)
-	material.albedo_color = Color("14ff00")
-	get_parent().get_node("triggers/t2/MeshInstance").set_surface_material(0, material)
-	
-	get_parent().get_node("triggers/t2").set_collision_layer_bit(0,false)
-	get_parent().get_node("triggers/t2").set_collision_mask_bit(0,false)
+#func _on_Abyss_body_entered(body):
+#	if body.name == "KineBall":
+#		get_tree().change_scene("res://scenes/Level.tscn")
+#
+#
+#func _on_Enemy_body_entered(body):
+#	if body.name == "KineBall":
+#		get_parent().get_node("Sounds/hurt").play()
+#		$Timer2.start()
+#
+#
+#func _on_finish_area_body_entered(body):	
+#	get_parent().get_node("Sounds/win").play()
+#	$Timer.start()
+#
+#
+#func _on_Timer_timeout():
+#	get_tree().change_scene("res://scenes/Control.tscn")
+#
+#
+#func _on_Timer2_timeout():
+#	get_tree().change_scene("res://scenes/Level.tscn")
+#
+#
+#func _on_Coin_cn_collected():
+#	get_parent().get_node("Sounds/coinsnd").play()
+#
+#
+#func _on_t1_body_entered(body):
+#	if body.name == "KineBall":
+#		get_parent().get_node("Doors/door1/door1animp").play("d1animp")
+##		
+#		var material = get_parent().get_node("triggers/t1/MeshInstance").get_surface_material(0)
+#		material.albedo_color = Color("14ff00")
+#		get_parent().get_node("triggers/t1/MeshInstance").set_surface_material(0, material)
+#
+#		get_parent().get_node("triggers/t1").set_collision_layer_bit(0,false)
+#		get_parent().get_node("triggers/t1").set_collision_mask_bit(0,false)
+#
+#
+#
+#func _on_t2_body_entered(body):
+#	get_parent().get_node("Doors/door2/leftside/AnimationPlayer").play("d2lsanimp")
+#	get_parent().get_node("Doors/door2/rightside/AnimationPlayer").play("d2rsanimp")
+#
+#	var material = get_parent().get_node("triggers/t2/MeshInstance").get_surface_material(0)
+#	material.albedo_color = Color("14ff00")
+#	get_parent().get_node("triggers/t2/MeshInstance").set_surface_material(0, material)
+#
+#	get_parent().get_node("triggers/t2").set_collision_layer_bit(0,false)
+#	get_parent().get_node("triggers/t2").set_collision_mask_bit(0,false)
