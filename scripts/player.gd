7bfd99bd2db89b05ce41eb2de839363af23cb681
TurBoss <j.l.toledano.l@gmail.com>
2021-10-26 13:21:01 +0200

    put player in scene and fix script animation names

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,7 +10,6 @@ var falling_fast = false
 var no_move_horizontal_time = 0.0
 
 onready var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-#var wind = 0.0
 
 var rotspeed = 10
 
@@ -112,7 +111,7 @@ func _physics_process(delta):
 			player_rotation = delta * rotspeed
 			$player.rotate(Vector3(0, 1, 0), player_rotation)
 		if jumps_count == 0:
-			$player/AnimationPlayer.play("run_forward-loop")
+			$player/AnimationPlayer.play("Run")
 			$player/AnimationPlayer.set_speed_scale(Input.get_action_strength("ui_left") * run_scale)
 			
 
@@ -123,7 +122,7 @@ func _physics_process(delta):
 			player_rotation = delta * rotspeed
 			$player.rotate(Vector3(0, 1, 0), player_rotation)
 		if jumps_count == 0:
-			$player/AnimationPlayer.play("run_forward-loop")
+			$player/AnimationPlayer.play("Run")
 			$player/AnimationPlayer.set_speed_scale(Input.get_action_strength("ui_right") * run_scale)
 			
 
@@ -160,7 +159,7 @@ func _physics_process(delta):
 			$player.rotate(Vector3(0, 1, 0), player_rotation)
 
 		if jumps_count == 0:
-			$player/AnimationPlayer.play("walk_forward-loop")
+			$player/AnimationPlayer.play("Walk")
 			$player/AnimationPlayer.set_speed_scale(Input.get_action_strength("ui_left") * walk_scale)
 			
 
@@ -172,7 +171,7 @@ func _physics_process(delta):
 			player_rotation = delta * rotspeed
 			$player.rotate(Vector3(0, 1, 0), player_rotation)
 		if jumps_count == 0:
-			$player/AnimationPlayer.play("walk_forward-loop")
+			$player/AnimationPlayer.play("Walk")
 			$player/AnimationPlayer.set_speed_scale(Input.get_action_strength("ui_right") * walk_scale)
 
 	# Crouch
@@ -190,7 +189,7 @@ func _physics_process(delta):
 
 
 	else:
-		$player/AnimationPlayer.play("idle-loop")
+		$player/AnimationPlayer.play("Idle")
 		velocity.z = lerp(velocity.z,0,0.2)
 
 
@@ -221,58 +220,6 @@ func _physics_process(delta):
 		$player.rotate(Vector3(0, 1, 0), delta * rotspeed)
 		$player/AnimationPlayer.play("walk_forward-loop")
 
-	# wind = rand_generate.randf_range(-1.0, 1.0)
 
 	velocity = move_and_slide(Vector3(0.0, velocity.y, velocity.z), Vector3.UP)
-
-	#vec_pos.z += wind
-
-#func _on_Enemy_body_entered(body):
-#	if body.name == "KineBall":
-#		get_parent().get_node("Sounds/hurt").play()
-#		$Timer2.start()
-#
-#
-#func _on_finish_area_body_entered(body):	
-#	get_parent().get_node("Sounds/win").play()
-#	$Timer.start()
-#
-#
-#func _on_Timer_timeout():
-#	get_tree().change_scene("res://scenes/Control.tscn")
-#
-#
-#func _on_Timer2_timeout():
-#	get_tree().change_scene("res://scenes/Level.tscn")
-#
-#
-#func _on_Coin_cn_collected():
-#	get_parent().get_node("Sounds/coinsnd").play()
-#
-#
-#func _on_t1_body_entered(body):
-#	if body.name == "KineBall":
-#		get_parent().get_node("Doors/door1/door1animp").play("d1animp")
-##		
-#		var material = get_parent().get_node("triggers/t1/MeshInstance").get_surface_material(0)
-#		material.albedo_color = Color("14ff00")
-#		get_parent().get_node("triggers/t1/MeshInstance").set_surface_material(0, material)
-#
-#		get_parent().get_node("triggers/t1").set_collision_layer_bit(0,false)
-#		get_parent().get_node("triggers/t1").set_collision_mask_bit(0,false)
-#
-#
-#
-#func _on_t2_body_entered(body):
-#	get_parent().get_node("Doors/door2/leftside/AnimationPlayer").play("d2lsanimp")
-#	get_parent().get_node("Doors/door2/rightside/AnimationPlayer").play("d2rsanimp")
-#
-#	var material = get_parent().get_node("triggers/t2/MeshInstance").get_surface_material(0)
-#	material.albedo_color = Color("14ff00")
-#	get_parent().get_node("triggers/t2/MeshInstance").set_surface_material(0, material)
-#
-#	get_parent().get_node("triggers/t2").set_collision_layer_bit(0,false)
-#	get_parent().get_node("triggers/t2").set_collision_mask_bit(0,false)
-
-
 
